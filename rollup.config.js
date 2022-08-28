3b42269f5a7c2c9f3793af8b75a31418eba8f12a
John Kreitlow <863023+radium-v@users.noreply.github.com>
2022-08-27 21:56:43 -0700

    add rollup and typescript config

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import CommonJS from "@rollup/plugin-commonjs";
+import { string } from "rollup-plugin-string";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      dir: "dist",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    nodeResolve({
+      mainFields: ["browser", "module", "main", "exports"],
+    }),
+    CommonJS(),
+    json(),
+    typescript(),
+    string({ include: "**/*.css" }),
+  ],
+};
