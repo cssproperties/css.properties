0ab7cc58b7a4a55479f74af83b6ba99ff970511d
John Kreitlow <863023+radium-v@users.noreply.github.com>
2023-01-31 22:15:53 -0800

    add index route

--- server/index.mjs
+++ server/index.mjs
@@ -7,6 +7,10 @@ const app = express();
 app.use("/dist", express.static("dist"));
 app.use("/", express.static("public"));
 
+app.get("*", (req, res) => {
+    res.sendFile("public/index.html", { root: "." });
+});
+
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
